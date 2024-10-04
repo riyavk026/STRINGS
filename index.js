6536dae4d814355ecf4ec43b2cc462c502fd6579
Riya <riyavk026@gmail.com>
2024-10-04 11:59:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("thi is strings tutorial")
+
+let a = "harry";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+let real_name ="harry"
+let friend = "riya"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b ="shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+
+console.log(b.replace("sh", "77"))
+console.log(b.concat(a,"riya" ,"priya","rahul" ))
+
+console.log(b)
